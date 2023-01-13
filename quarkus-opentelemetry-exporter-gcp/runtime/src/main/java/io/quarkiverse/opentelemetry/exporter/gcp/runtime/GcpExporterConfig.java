14ac8ac4384a88615d9b07bff0f492a27f3730b7
Anton Yakimov <anton.jakimov@gmail.com>
2023-01-13 23:10:10 +0200

    chore: Documentation updates.

--- quarkus-opentelemetry-exporter-gcp/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/gcp/runtime/GcpExporterConfig.java
+++ quarkus-opentelemetry-exporter-gcp/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/gcp/runtime/GcpExporterConfig.java
@@ -12,9 +12,7 @@ public class GcpExporterConfig {
     @ConfigRoot(name = "opentelemetry.tracer.exporter.gcp", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
     public static class GcpExporterBuildConfig {
         /**
-         * GCP Opentelemetry trace exporter support.
-         * <p>
-         * GCP Opentelemetry support is enabled by default.
+         * GCP's Tracing exporter support. Enabled by default.
          */
         @ConfigItem(defaultValue = "true")
         public Boolean enabled;
@@ -23,23 +21,23 @@ public class GcpExporterConfig {
     @ConfigRoot(name = "opentelemetry.tracer.exporter.gcp", phase = ConfigPhase.RUN_TIME)
     public static class GcpExporterRuntimeConfig {
         /**
-         * Set GCP Project ID.
+         * Override to set GCP's projectid.
          */
         @ConfigItem
         @ConvertWith(TrimmedStringConverter.class)
         public Optional<String> projectid;
 
         /**
-         * Override for GCP TraceEndpoint. Optional, usually is set by GCP's TraceConfiguration library.
+         * Override for GCP TraceEndpoint setting.
          */
         @ConfigItem
         @ConvertWith(TrimmedStringConverter.class)
         public Optional<String> endpoint;
 
         /**
-         * Support for Cloud Run environments. Cloud Run doesn't support background processes so we need to use different
-         * SpanProcessor.
-         * <a href="https://cloud.google.com/trace/docs/setup/java-ot#export">Reference</a>
+         * Support for Cloud Run environments. Set to `true` for Cloud Run deployments.
+         * <p>
+         * Cloud Run <a href="https://cloud.google.com/trace/docs/setup/java-ot#export">doesn't support background processes</a> and `SimpleSpanProcessor` must be used.
          */
         @ConfigItem(defaultValue = "false")
         public Boolean cloudrun;
